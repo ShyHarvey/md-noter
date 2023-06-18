2422aec28c202ae474cca20cbf599b2d8cedc1fc
Ilya Andreichenko <ilia.andrejchenko@gmail.com>
2023-06-18 13:49:13 +0300

    add google auth

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com"]
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"]
   },
 
   /**
